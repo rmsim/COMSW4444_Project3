c5796c49fc0e0d2a9dffb2f2b5c4b30eaa74c132
Rui Min <rmsim@dyn-160-39-133-227.dyn.columbia.edu>
2017-11-06 14:19:57 -0500

    Putting our files back with new similator

--- exchange/g3/Player.java
+++ exchange/g3/Player.java
@@ -8,7 +8,6 @@ import exchange.sim.*;
 public class Player extends exchange.sim.Player {
     /*
         Inherited from exchange.sim.Player:
-
         Remark: you have to manually adjust the order of socks, to minimize the total embarrassment
                 the score is calculated based on your returned list of getSocks(). Simulator will pair up socks 0-1, 2-3, 4-5, etc.
      */
@@ -16,20 +15,18 @@ public class Player extends exchange.sim.Player {
     private Sock[] socks;
     private SockCollection socksCollection;
     private int turn = -1;
-    private int round = 0;
-    private Boolean transactionSuccess = false;
+    private int timeSinceTransaction = 0;
+    private boolean transactionOccurred = true;
+
     private RoundCollection rounds;
-    private int n;
 
     @Override
     public void init(int id, int n, int p, int t, List<Sock> socks) {
         this.id = id;
-        this.n = n;
         this.socks = socks.toArray(new Sock[2 * n]);
-        this.socksCollection = new SockCollection(this.socks);
+        this.socksCollection = new SockCollection(this.socks, id);
         this.rounds = new RoundCollection();
     }
-
 
     @Override
     public Offer makeOffer(List<Request> lastRequests, List<Transaction> lastTransactions) {
@@ -40,17 +37,22 @@ public class Player extends exchange.sim.Player {
 
         // Tracks the current turn number.
         this.turn += 1;
-        
+
+        if (!transactionOccurred) {
+            ++timeSinceTransaction;
+        }
+
+        transactionOccurred = false;
+
+        if (timeSinceTransaction > 3) {
+            this.socksCollection.shuffle();
+        }
+
         rounds.putTransactionInfo(lastRequests, lastTransactions);
 
         int[] worstPairIds = this.socksCollection.getWorstPairIds();
-        //this.id1 = worstPairIds[(round + 0) % (2*n)];
-        //this.id2 = worstPairIds[(round + 1) % (2*n)] ;
-
         this.id1 = worstPairIds[0];
-        this.id2 = worstPairIds[1] ;
-
-        
+        this.id2 = worstPairIds[1];
         return new Offer(
                 this.socksCollection.getSock(this.id1),
                 this.socksCollection.getSock(this.id2));
@@ -64,37 +66,11 @@ public class Player extends exchange.sim.Player {
 			offer.getSock(rank = 1, 2)		-		get rank's offer
 			offer.getFirst()				-		equivalent to offer.getSock(1)
 			offer.getSecond()				-		equivalent to offer.getSock(2)
-
 			Remark: For Request object, rank ranges between 1 and 2
 		 */
-		rounds.putOfferInfo(offers);
 
-		List<Integer> availableOffers = new ArrayList<>();
-		for (int i = 0; i < offers.size(); ++ i) {
-		    if (i == id) continue;
-
-		    // Encoding the offer information into integer: id * 2 + rank - 1
-            if (offers.get(i).getFirst() != null)
-                availableOffers.add(i * 2);
-            if (offers.get(i).getSecond() != null)
-                availableOffers.add(i * 2 + 1);
-        }
-
-        int test = random.nextInt(3);
-        if (test == 0 || availableOffers.size() == 0) {
-            // In Request object, id == -1 means no request.
-            return new Request(-1, -1, -1, -1);
-        } else if (test == 1 || availableOffers.size() == 1) {
-            // Making random requests
-            int k = availableOffers.get(random.nextInt(availableOffers.size()));
-            return new Request(k / 2, k % 2 + 1, -1, -1);
-        } else {
-            int k1 = availableOffers.get(random.nextInt(availableOffers.size()));
-            int k2 = availableOffers.get(random.nextInt(availableOffers.size()));
-            while (k1 == k2)
-                k2 = availableOffers.get(random.nextInt(availableOffers.size()));
-            return new Request(k1 / 2, k1 % 2 + 1, k2 / 2, k2 % 2 + 1);
-        }
+        rounds.putOfferInfo(offers);
+        return socksCollection.requestBestOffer(offers);
     }
 
     @Override
@@ -106,7 +82,6 @@ public class Player extends exchange.sim.Player {
             transaction.getSecondRank()     -       Similar as above
             transaction.getFirstSock()      -       Sock offered by the first player
             transaction.getSecondSock()     -       Similar as above
-
             Remark: rank ranges between 1 and 2
          */
         int rank;
@@ -121,17 +96,13 @@ public class Player extends exchange.sim.Player {
 
         if (rank == 1) {
             socksCollection.putSock(id1, newSock);
-            transactionSuccess = true;            
         }
-        else{ 
+        else {
             socksCollection.putSock(id2, newSock);
-            transactionSuccess = false; 
         }
 
-        if(transactionSuccess)
-            round = 0;
-        else 
-            round = round + 2;
+        transactionOccurred = true;
+        timeSinceTransaction = 0;
     }
 
     @Override
--- exchange/g3/SockCollection.java
+++ exchange/g3/SockCollection.java
@@ -1,163 +1,260 @@
 package exchange.g3;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Random;
+
+import exchange.sim.Offer;
+import exchange.sim.Request;
 import exchange.sim.Sock;
 
 public class SockCollection{
 
     ArrayList<Sock> collection;
-    int embarrassment;
+    double maxDist = 442.0;
+    int id; // Player Id.
 
+    // Keeps track of whether a transaction has occurred to
+    //  optimize against performing greedy pair.
+    boolean transactionOccurred = true;
 
-    public SockCollection(){
-        collection = new ArrayList<Sock>(); 
-        embarrassment = 0;
-    }
+    int K; // for clustering
+    ArrayList<ArrayList<Sock>> clusters;
+    double[][] clusterCenters;
 
-    public SockCollection(ArrayList<Sock> collection){
-        this.collection = collection;
-        embarrassment = 0;
-    }
+    public SockCollection(Sock[] socks, int id){
+        this.id = id;
+        this.collection = new ArrayList<>(Arrays.asList(socks));
 
+        this.K = 8;
+        this.clusters = new ArrayList();
+        for (int i = 0; i < K; i++) {
+            clusters.add(new ArrayList<Sock>());
+        }
 
-    public SockCollection(Sock[] socks){
-        this.collection = new ArrayList<Sock>(Arrays.asList(socks));
-        embarrassment = 0;
+        this.clusterCenters = new double[K][3];
     }
 
     public void addSock(Sock newSock){
         collection.add(newSock);
     }
 
-
     public void removeSock(Sock unwantedSock){
         collection.remove(unwantedSock);
     }
 
-
-    private ArrayList<ArrayList<Sock>> getSockBuckets() {
-        ArrayList<ArrayList<Sock>> rgb = new ArrayList<>();
-
-        char[] colors = new char[] {'R', 'G', 'B'};
-
-        for (int i=0; i<3; ++i) {
-            rgb.add(sortSockBucket(getSocksOfAColor(colors[i])));
-        }
-
-        return rgb;
+    public static double findDist(Sock s, double[] p) {
+        double x1 = (double)s.R;
+        double y1 = (double)s.G;
+        double z1 = (double)s.B;
+        double x2 = p[0];
+        double y2 = p[1];
+        double z2 = p[2];
+        return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
     }
 
-    private ArrayList<Sock> sortSockBucket(ArrayList<Sock> socks) {
-        Collections.sort(socks, new Comparator<Sock>(){
-            @Override
-            public int compare(Sock sock1, Sock sock2){
-                // Weird messing sorting algorithm because I am
-                //  unable to find a way to use the bucket color
-                //  here directly. Performing reflection for each iteration
-                //  was an over-kill.
-                if (sock1.R >= sock1.G && sock1.R >= sock1.B)
-                    return sock2.R - sock1.R;
+    public double[][] findInitialClusterCenters() {
+        // Change the order of socks in your collection using some algorithm.
+        Random rand = new Random();
+        double[][] ret = new double[K][3];
+        for (int k = 0; k < K; k++) {
 
-                if (sock1.G > sock1.R && sock1.G >= sock1.B)
-                    return sock2.G - sock1.G;
-
-                return sock2.B - sock1.B;
-            }
-        });
-
-        return socks;
+            ret[k][0] = 255*rand.nextDouble();
+            ret[k][1] = 255*rand.nextDouble();
+            ret[k][2] = 255*rand.nextDouble();
+        }
+        return ret;
     }
 
-    private ArrayList<Sock> getSocksOfAColor(char color) {
-        ArrayList<Sock> socks = new ArrayList<>();
-
-        for (Sock s : collection) {
-            switch (color) {
-                case 'R':
-                    if (s.R >= s.B && s.R >= s.G) {
-                        socks.add(s);
+    private void cluster(int numIterations) {
+        // adjusts clusters and clusterCenters based on collection
+        clusterCenters = findInitialClusterCenters();
+        int bestClusterNumber;
+        double closestDistance;
+        double d;
+        for (int iteration = 0; iteration < numIterations; iteration++) {
+            // use centers to place socks into clusters
+            for (ArrayList<Sock> c : clusters) {
+                c.clear();
+            }
+            for (Sock s : collection) {
+                bestClusterNumber = -1;
+                closestDistance = maxDist;
+                for (int k = 0; k < K; k++) {
+                    d = findDist(s, clusterCenters[k]);
+                    if (d < closestDistance) {
+                        closestDistance = d;
+                        bestClusterNumber = k;
                     }
-
-                    break;
-
-                case 'G':
-                    if (s.G > s.R && s.G >= s.B) {
-                        socks.add(s);
-                    }
-
-                    break;
-
-                case 'B':
-                    if (s.B > s.R && s.B > s.G) {
-                        socks.add(s);
-                    }
-
-                    break;
-
-                default:
-                    System.err.println("Unknown color!");
+                }
+                clusters.get(bestClusterNumber).add(s);
+            }
+            // use clusters to adjust centers
+            for (int k = 0; k < K; k++) {
+                clusterCenters[k] = meanLoc(clusters.get(k));
             }
         }
+    }
 
-        return socks;
+    public static double[] meanLoc(ArrayList<Sock> cluster) {
+        // returns the average location of a cluster (the "true" center)
+        int clusterSize = cluster.size();
+        double x = 0.0;
+        double y = 0.0;
+        double z = 0.0;
+        for (Sock s : cluster) {
+            x += (double) s.R;
+            y += (double) s.G;
+            z += (double) s.B;
+        }
+        x /= clusterSize;
+        y /= clusterSize;
+        z /= clusterSize;
+        return new double[]{x, y, z};
+    }
+
+    public void greedilyProcessSockList(ArrayList<Sock> socks) {
+        ArrayList<Sock> processedSocks = new ArrayList<Sock>();
+        while (socks.size() >= 2) {
+            Sock s = socks.get(0);
+            socks.remove(s);
+
+            double smallest_dist = maxDist;
+            Sock bestSock = null;
+
+            for (Sock s2 : socks) {
+                double d = s.distance(s2);
+                if (d < smallest_dist) {
+                    smallest_dist = d;
+                    bestSock = s2;
+                }
+            }
+
+
+            processedSocks.add(s);
+            processedSocks.add(bestSock);
+            socks.remove(bestSock);
+        }
+
+        socks.addAll(processedSocks);
     }
 
     // Change the order of socks in your collection using some algorithm.
     private void preprocessSockCollection() {
-        ArrayList<ArrayList<Sock>> rgb = getSockBuckets();
-        collection.clear();
+        cluster(30);
 
-        for (ArrayList<Sock> bucket : rgb) {
-            collection.addAll(bucket);
+        // System.out.println("PRINTING CLUSTERS:");
+        // for (ArrayList<Sock> c : clusters) {
+        //     for (Sock s : c) {
+        //         System.out.println(s);
+        //     }
+        //     System.out.println();
+        // }
+
+
+        for (ArrayList<Sock> c : clusters) {
+            greedilyProcessSockList(c);
         }
+
+        ArrayList<Sock> extras = new ArrayList<Sock>();
+
+        collection.clear();
+        for (ArrayList<Sock> c : clusters) {
+            if (c.size() % 2 == 0) {
+                collection.addAll(c);
+            } else {
+                for (int i = 1; i < c.size(); i++) {
+                    collection.add(c.get(i));
+                }
+                extras.add(c.get(0));
+            }
+        }
+        collection.addAll(extras);
+
+
+
+
+        // greedilyProcessSockList(collection);
+
     }
 
-    
+    private void sortByDistance() {
+        for (int i = 0; i < collection.size()-2; i+=2) {
+            int j = i+2;
+            Sock s2 = collection.get(j+1);
+            Sock s1 = collection.get(j);
+            double currentDist = s1.distance(s2);
+
+            j-=2;
+            while (j >= 0 && currentDist > collection.get(j).distance(collection.get(j+1))) {
+                collection.set(j+3, collection.get(j+1));
+                collection.set(j+2, collection.get(j));
+                j-=2;
+            }
+
+            collection.set(j+2, s1);
+            collection.set(j+3, s2);
+        }
+    }
 
     private int[] getWorstPairingSockIds() {
-        int w1 = -1;
-        int w2 = -1;
+        // int w1 = -1;
+        // int w2 = -1;
 
-        double maxDist = -1.0;
+        // double maxDistance = -1.0;
 
-        for (int i = 0; i < collection.size(); i += 2) {
-            Sock sock1 = collection.get(i);
-            Sock sock2 = collection.get(i + 1);
+        // for (int i = 0; i < collection.size(); i += 2) {
+        //     Sock sock1 = collection.get(i);
+        //     Sock sock2 = collection.get(i + 1);
 
-            if (sock1.distance(sock2) > maxDist) {
-                w1 = i;
-                w2 = i+1;
-                maxDist = sock1.distance(sock2);
+        //     if (sock1.distance(sock2) > maxDistance) {
+        //         w1 = i;
+        //         w2 = i+1;
+        //         maxDistance = sock1.distance(sock2);
+        //     }
+        // }
+
+        // return new int[] { w1, w2 };
+
+        return new int[] {collection.size()-1, collection.size()-2};
+    }
+
+    // Gets the shortest distance of a sock to the rest of our collection.
+    private double getShortestDistance(Sock sock) {
+        double minDistance = maxDist;
+        for (int i = 0; i < collection.size(); ++i) {
+            if (sock.distance(collection.get(i)) < minDistance) {
+                minDistance = sock.distance(collection.get(i));
             }
         }
 
-        return new int[] { w1, w2 };
+        return minDistance;
     }
 
-    public void sortSocks(){
-        Collections.sort(collection, new Comparator<Sock>(){
-            @Override
-            public int compare(Sock sock1, Sock sock2){
-                //Descending Order
-                return sock2.hashCode() - sock1.hashCode();             
+    // Check if a new sock will give us a better pairing than an existing pair.
+    private boolean checkNewSockCompatibility(Sock sock) {
+        for (int i = 0; i < collection.size(); i += 2) {
+            // int j = i % 2 == 0? i + 1: i - 1;
+            int j = i + 1;
+            if (sock.distance(collection.get(i)) <
+                    collection.get(i).distance(collection.get(j))) {
+                return true;
             }
-        });
-    }    
+        }
 
+        return false;
+    }
 
-    public void computeEmbarrassment(){
-        preprocessSockCollection();
-        //sortSocks();
+    public double computeEmbarrassment(){
+        if (transactionOccurred)
+            preprocessSockCollection();
 
         if(collection.size() % 2 != 0){
-            embarrassment = -1;
-            return;
-        }                
-               
-        int result = 0;
+            return -1.0;
+        }
+
+        double result = 0;
         for(int i = 0; i < collection.size(); i += 2){
             Sock sock1 = collection.get(i);
             Sock sock2 = collection.get(i + 1);
@@ -165,19 +262,20 @@ public class SockCollection{
             result += sock1.distance(sock2);
         }
 
-        this.embarrassment = result;
-    }
-
-    public int getEmbarrassment(){
-        computeEmbarrassment();
-        return this.embarrassment;
+        return result;
     }
 
     public int[] getWorstPairIds(){
-        computeEmbarrassment();
-        
-        if (collection.size() < 2)
+        if (transactionOccurred) {
+            preprocessSockCollection();
+        }
+
+        if (collection.size() < 2) {
             return new int[]{-1, -1};
+        }
+
+        // Reset the transaction flag.
+        transactionOccurred = false;
 
         return getWorstPairingSockIds();
     }
@@ -187,8 +285,78 @@ public class SockCollection{
     }
 
     public void putSock(int id, Sock s) {
-        collection.remove(id);
-        collection.add(s);
+        collection.set(id, s);
+        transactionOccurred = true;
+    }
+
+    // Shuffle socks around to get another sock to trade next time.
+    public void shuffle() {
+        int n = collection.size();
+
+        int i = 2 * collection.size() / 3;
+        int j = i%2 == 0? i + 1: i - 1;
+
+        Sock temp1 = collection.get(n-1);
+        Sock temp2 = collection.get(n-2);
+
+        collection.set(n-1, collection.get(i));
+        collection.set(n-2, collection.get(j));
+
+        collection.set(i, temp1);
+        collection.set(j, temp2);
+
+        transactionOccurred = true;
+    }
+
+    public Request requestBestOffer(List<Offer> offers) {
+
+        Offer myOffer = offers.get(id);
+
+        // Removing socks from our collection and adding them back in later.
+        int s1Index = collection.indexOf(myOffer.getFirst());
+        collection.remove(myOffer.getFirst());
+        int s2Index = collection.indexOf(myOffer.getSecond());
+        collection.remove(myOffer.getSecond());
+
+        List<Integer> toPickOffer = new ArrayList<>(Arrays.asList(-1, -1));
+        List<Integer> toPickRank = new ArrayList<>(Arrays.asList(-1, -1));
+
+        for (int i = 0; i < offers.size(); ++i) {
+            if (toPickOffer.get(0) != -1 && toPickOffer.get(1) != -1) {
+                // We have already selected socks.
+                // We're not going to look at the rest of the offers.
+                break;
+            }
+
+            // Skip our socks.
+            if (i == id) {
+                continue;
+            }
+
+            for (int j = 1; j < 3; ++j) {
+                if (offers.get(i).getSock(j) == null) {
+                    // I'm not sure what condition to check for if the player has not offered
+                    //  a sock.
+                    continue;
+                }
+
+                if (checkNewSockCompatibility(offers.get(i).getSock(j))){
+                    if (toPickOffer.get(0) == -1) {
+                        toPickOffer.set(0, i);
+                        toPickRank.set(0, j);
+                    }
+                    else if (toPickOffer.get(1) == -1) {
+                        toPickOffer.set(1, i);
+                        toPickRank.set(1, j);
+                    }
+                }
+            }
+        }
+
+        collection.add(s2Index, myOffer.getSecond());
+        collection.add(s1Index, myOffer.getFirst());
+
+        return new Request(toPickOffer.get(0), toPickRank.get(0), toPickOffer.get(1), toPickRank.get(1));
     }
 
     public ArrayList<Sock> getCollection() {
